a294fbee2c3b437f5a4c7bc851e2e92e0b394e7e
Alonso Heynes <alonso.heynes@gmail.com>
2019-04-24 11:12:12 -0500

    commit
    Add new items to the todolist database. Delete items from the todolist database.

--- app.js
+++ app.js
@@ -56,16 +56,29 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const item = new Item ({
+    name: itemName
+  }); 
+  item.save();
+  res.redirect("/");
 });
+
+app.post("/delete", function(req, res){
+  const checkItemId = req.body.checkbox;
+  
+  Item.findByIdAndRemove(checkItemId, function(err){
+    if(!err){
+      console.log("Successfully deleted item.");
+      res.redirect("/");
+    }
+  });
+
+});
+
+
+
 
 app.get("/work", function(req,res){
   res.render("list", {listTitle: "Work List", newListItems: workItems});
